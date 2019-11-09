d30af4fa23dc2000c6e35569fd70dbd519f5436c
StudioAlpha <studioalpha@protonmail,com>
2019-11-08 23:43:34 -0800

    Game Default Scene Updated

--- Scenes/Screen/Screen.gd
+++ Scenes/Screen/Screen.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	pass
-
-func _on_BootScreen_animation_finished():
-	pass
+	$BootScreen.play("Default")
+	$BootScreen.connect("animation_finished", self, "Boot_finished")
+	
+func Boot_finished(animation):
+	free()
