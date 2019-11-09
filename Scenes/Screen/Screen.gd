f27c438b7ed00da0faf725f58d30206dcff0b3aa
StudioAlpha <studioalpha@protonmail,com>
2019-11-08 23:57:57 -0800

    Update Screen.gd

--- Scenes/Screen/Screen.gd
+++ Scenes/Screen/Screen.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	$BootScreen.play("Default")
-	$BootScreen.connect("animation_finished", self, "Boot_finished")
-	
-func Boot_finished(animation):
-	free()
+	pass
+
+func _on_BootScreen_animation_finished():
+	$BootScreen.hide()
