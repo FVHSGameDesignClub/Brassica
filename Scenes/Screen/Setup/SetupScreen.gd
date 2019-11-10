070088863111d6e5bc338ab00c62c17fa6cf3287
jeongwonkim1215 <jeongwonkim031215@gmail.com>
2019-11-09 22:43:30 -0800

    SetupScreen Updated

--- Scenes/Screen/Setup/SetupScreen.gd
+++ Scenes/Screen/Setup/SetupScreen.gd
@@ -1,7 +1,31 @@
 extends Node2D
 
-var Step = 0
+var step = 0
 
 func _ready():
 	pass
+	
+func _process(delta):
+	if step == 0:
+		Turn_LanguageSetup()
+	elif step == 1:
+		Turn_PresentScreen()
+	pass
+	
+func _on_Previous_pressed():
+	step - 1
+	pass
 
+func _on_Next_pressed():
+	step + 1
+	pass
+
+func Turn_LanguageSetup():
+	$LanguageSetup.show()
+	$PresentScreen.hide()
+	$Control/HBox/Previous.hide()
+	
+func Turn_PresentScreen():
+	$LanguageSetup.hide()
+	$PresentScreen.show()
+	$Control/HBox/Previous.show()
