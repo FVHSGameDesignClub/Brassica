e18bdce29fea57ae0b243217c38907e51b87836c
jeongwonkim1215 <jeongwonkim031215@gmail.com>
2019-11-10 22:14:42 -0800

    Update SetupScreen.gd

--- Scenes/Screen/Setup/SetupScreen.gd
+++ Scenes/Screen/Setup/SetupScreen.gd
@@ -5,39 +5,46 @@ var step = 0
 var LANGUAGE = "English"
 
 func _ready():
+	
 	pass
 	
 func _process(delta):
 	if step == 0:
 		Turn_LanguageSetup()
+
 	elif step == 1:
 		Turn_PresentScreen()
+		
 	elif step == 2:
 		Turn_TermsAndConditions()
+		
 	elif step == 3:
 		Turn_SettingUp()
+		
 	pass
 	
-	if get_node("LanguageSetup")._on_English_pressed():
-		LANGUAGE = "English"
-	if get_node("LanguageSetup")._on_Spanish_pressed():
-		LANGUAGE = "Spanish"
-	if get_node("LanguageSetup")._on_Korean_pressed():
-		LANGUAGE = "Korean"
+	$PresentScreen/Banner.play(LANGUAGE)
 	
+	$TermsAndConditions/Banner.play(LANGUAGE)
+	
+	$SettingUp/Banner.play(LANGUAGE)
+
+
 func _on_Next_pressed():
 	step += 1
 
 func _on_Previous_pressed():
 	step -= 1
-	
+
 
 func Turn_LanguageSetup():
 	$PresentScreen.hide()
 	$SettingUp.hide()
 	$TermsAndConditions.hide()
 	$LanguageSetup.show()
+	
 	$Control/HBox/Previous.hide()
+	
 	pass
 
 func Turn_PresentScreen():
@@ -46,7 +53,6 @@ func Turn_PresentScreen():
 	$TermsAndConditions.hide()
 	$PresentScreen.show()
 	$Control/HBox/Previous.show()
-	$PresentScreen/Banner.play(LANGUAGE)
 	pass
 	
 func Turn_TermsAndConditions():
@@ -54,7 +60,6 @@ func Turn_TermsAndConditions():
 	$SettingUp.hide()
 	$PresentScreen.hide()
 	$TermsAndConditions.show()
-	$TermsAndConditions/Banner.play(LANGUAGE)
 	pass
 	
 func Turn_SettingUp():
@@ -63,7 +68,6 @@ func Turn_SettingUp():
 	$TermsAndConditions.hide()
 	$SettingUp.show()
 	$SettingUp/Animation.play("default")
-	$SettingUp/Banner.play(LANGUAGE)
 	
 	$Control.hide()
 	pass
