77943e17ed54c647a190a9941747ea460bc49c65
jeongwonkim1215 <jeongwonkim031215@gmail.com>
2019-11-21 22:58:23 -0800

    Update Main.gd

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -1,18 +1,28 @@
 extends Node2D
 
+var config_exists = false
 
 func _ready():
-	pass
+	check_config_file_exist()
 
 func _process(delta):
-	
-	var is_Setup_Over = get_node("SetupScreen/SettingUp").is_setup_over
-	
-	if is_Setup_Over == true:
-		setup_is_over()
+	if config_exists:
+		get_node("SetupScreen").hide()
 		
-	elif is_Setup_Over == false:
-		pass
+	elif !config_exists:
+		$SetupScreen.show()
+		
+		var is_Setup_Over = get_node("SetupScreen/SettingUp").is_setup_over
+		
+		if is_Setup_Over == true:
+			get_node("SetupScreen").hide()
 
-func setup_is_over():
-	$SetupScreen.hide()
+
+func check_config_file_exist():
+	var file = File.new()
+	var cfgExists = file.file_exists("user://data/settings.cfg")
+	
+	if cfgExists:
+		config_exists = true
+	elif !cfgExists:
+		config_exists = false
