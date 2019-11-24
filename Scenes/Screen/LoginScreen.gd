95e20f48382f8ee79c7e2b64809872b075de249d
jeongwonkim1215 <jeongwonkim031215@gmail.com>
2019-11-23 19:15:28 -0800

    Login Screen Updated

--- Scenes/Screen/LoginScreen.gd
+++ Scenes/Screen/LoginScreen.gd
@@ -1,2 +1,20 @@
 extends Node2D
 
+var Fullname
+var Password
+var Hint
+
+var data = ConfigFile.new()
+
+func _ready():
+	loadData()
+
+func _process(delta):
+	$VerticalCenter/HorizontalCenter/Center/Align/Label.set_text(Fullname)
+	pass
+
+func loadData():
+	data.load("user://data/user.cfg")
+	Fullname = data.get_value("Name", "Fullname")
+	Password = data.get_value("Security", "Password")
+	Hint = data.get_value("Security", "Hint")
